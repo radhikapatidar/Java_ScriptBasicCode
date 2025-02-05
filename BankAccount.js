fb1a73a69d037738adbef6e5ca5d9cf4be56f1c3
radhika patidar <radhikapatidar360@gmail.com>
2025-02-05 13:21:13 +0530

    change in bankAccount.js

--- BankAccount.js
+++ BankAccount.js
@@ -8,7 +8,7 @@ class BankAccount{
    //Method to deposite money
    deposite(amount){
     if(amount >0){
-        TimeRanges.balance +=amount;
+        this.balance +=amount;
         console.log(`Deposited : ${amount} New Balance : ${this.balance}`);
     }else{
         console.log("Deposite amount must be greater than zero");
@@ -17,7 +17,21 @@ class BankAccount{
    //Method to withdraw money
    withdraw(amount){
     if(amount >0 && amount <=this.balance){
-        this.balance
+        this.balance -= amount;
+        console.log(`Withdrawn : ${amount}. Remaining Balance : ${this.balance} `);
+    }else if(amount > this.balance){
+        console.log("Insufficent Balance");
+    }else{
+        console.log("Withdrawal amount must be greater than zero");
     }
-   }
+   
 }
+checkBalance() {
+    console.log(`current Balance : ${this.balance}`);
+}
+}
+const myAccount = new BankAccount('Radhika',1000);
+myAccount.checkBalance();
+myAccount.deposite(500);
+myAccount.withdraw(200);
+myAccount.checkBalance();
